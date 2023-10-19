ce320417e9e37cd342d8d18c4716da6dab2cf8e1
RowBowl <robelrb@gmail.com>
2023-10-19 02:36:15 -0400

    editing taskpane.js and taskpane.html to add a few buttons that add a paragraph, and edit style

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, Word */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("insert-paragraph").onclick = () => tryCatch(insertParagraph);
+    document.getElementById("apply-style").onclick = () => tryCatch(applyStyle);
+    document.getElementById("apply-custom-style").onclick = () => tryCatch(applyCustomStyle);
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("change-font").onclick = () => tryCatch(changeFont);
+  }
+});
+
+async function insertParagraph() {
+  await Word.run(async (context) => {
+    const docBody = context.document.body;
+    docBody.insertParagraph(
+      "Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+      Word.InsertLocation.start
+    );
+
+    await context.sync();
+  });
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+  try {
+    await callback();
+  } catch (error) {
+    // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+    console.error(error);
+  }
+}
+
+async function applyStyle() {
+  await Word.run(async (context) => {
+    const firstParagraph = context.document.body.paragraphs.getFirst();
+    firstParagraph.styleBuiltIn = Word.Style.intenseReference;
+
+    await context.sync();
+  });
+}
+
+async function applyCustomStyle() {
+  await Word.run(async (context) => {
+    const lastParagraph = context.document.body.paragraphs.getLast();
+    lastParagraph.style = "MyCustomStyle";
+
+    await context.sync();
+  });
+}
+
+async function changeFont() {
+  await Word.run(async (context) => {
+    const secondParagraph = context.document.body.paragraphs.getFirst().getNext();
+    secondParagraph.font.set({
+      name: "Courier New",
+      bold: true,
+      size: 18,
+    });
+
+    await context.sync();
+  });
+}
